bd63fa76341d5406c22e5552cfe7cf91bfbd6332
chris pryer <christophpryer@gmail.com>
2022-02-20 15:22:16 -0500

    using routes for socials

---
+++ app/constants/__init__.py
@@ -0,0 +1,1 @@
+from app.constants import socials
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, send_from_directory
+from flask import redirect, render_template, send_from_directory
 from app.main import bp
 from app.utils import url_for
+from app.constants import socials
 
 
 @bp.route("/favicon.ico")
@@ -11,7 +12,37 @@ def favicon():
     )
 
 
-@bp.route("/", methods=["GET", "POST"])
-@bp.route("/index", methods=["GET", "POST"])
+@bp.route("/", methods=["GET"])
+@bp.route("/index", methods=["GET"])
 def index():
     return render_template("index.html", title="cnpryer.com")
+
+
+@bp.route("/github", methods=["GET"])
+def github():
+    return redirect(socials.GITHUB_URL)
+
+
+@bp.route("/twitter", methods=["GET"])
+def twitter():
+    return redirect(socials.TWITTER_URL)
+
+
+@bp.route("/instagram", methods=["GET"])
+def instagram():
+    return redirect(socials.INSTAGRAM_URL)
+
+
+@bp.route("/devpost", methods=["GET"])
+def devpost():
+    return redirect(socials.DEVPOST_URL)
+
+
+@bp.route("/medium", methods=["GET"])
+def medium():
+    return redirect(socials.MEDIUM_URL)
+
+
+@bp.route("/linkedin", methods=["GET"])
+def linkedin():
+    return redirect(socials.LINKEDIN_URL)
